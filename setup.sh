aaf8c94154f1b4477b0d6281d61345a1a6174955
jerrygems <shubham.debian@gmail.com>
2024-05-03 23:13:07 +0530

    just putting the configs to the repo

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#ssh-keygen -t rsa -b 2048 -C "jerrygems" -N "your_password_here"
+#ssh-keygen -t rsa -b 2048 -C "ansible" -N "no_pass_for_passwordless_connection"
+tmux set -g mouse on
+tmux split-window -h
+tmux select-pane -t 0
+tmux split-window -p 20 -v
+tmux select-pane -t 2
+tmux split-window -v
+tmux send-keys -t 0 "clear;neofetch" C-m
+tmux send-keys -t 1 "cava" C-m
+tmux send-keys -t 2 "clear;sudo docker run -it dock2" C-m
+tmux send-keys -t 3 "clear;sudo docker run -it dock2" C-m
+
+# copying the public keys to the hosts so that connection becomes easier
+ssh-copy-id -i ~/.ssh/id_rsa.pub root@172.17.0.2
+ssh-copy-id -i ~/.ssh/ansible.pub root@172.17.0.3
